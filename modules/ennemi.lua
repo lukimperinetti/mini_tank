892dd25f508bb99c6d0874f9d84204a1ffde712a
Luk <luk.imperinetti@gmail.com>
2024-04-18 19:59:08 +0200

    Creating Ennemi | preview of state machine | little break to implement rotation on the player

--- modules/ennemi.lua
+++ modules/ennemi.lua
@@ -1,19 +1,46 @@
+-- Returns the angle between two vectors assuming the same origin.
+function math.angle(x1, y1, x2, y2) return math.atan2(y2 - y1, x2 - x1) end
+
+-- Returns the distance between two points
+function math.dist(x1, y1, x2, y2) return ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5 end
+
 local ennemi = {}
--- ennemi.image = love.graphics.newImage("src/images/tank.png")
-ennemi.x = math.random(0, love.graphics.getWidth())
-ennemi.y = math.random(0, love.graphics.getHeight())
-ennemi.speed = 50
-ennemi.vx = 0
-ennemi.vy = 0
+
+-- State machine ennemi
+local TSTATE = {}
+TSTATE.NONE = ""
+TSTATE.WALK = "move"
+TSTATE.ATTACK = "attack"
+TSTATE.BITE = "shot"
+TSTATE.CHANGEDIR = "changedir"
+
+
+-- Create ennemi
+function CreateEnnemis()
+    ennemi.image = love.graphics.newImage("src/images/ennemi.png")
+    ennemi.x = math.random(10, screenWidth - 10)
+    ennemi.y = math.random(10, (screenHeight / 2) - 10)
+
+    ennemi.speed = 50
+    ennemi.vx = 0
+    ennemi.vy = 0
+
+    ennemi.state = TSTATE.NONE
+end
 
 function ennemi.load()
     print("ennemi loaded")
+    screenWidth = love.graphics.getWidth()
+    screenHeight = love.graphics.getHeight()
+    CreateEnnemis()
 end
 
 function ennemi.update(dt)
+
 end
 
 function ennemi.draw()
+    love.graphics.draw(ennemi.image, ennemi.x, ennemi.y)
 end
 
 return ennemi
--- scenes/sceneGameplay.lua
+++ scenes/sceneGameplay.lua
@@ -3,18 +3,22 @@ local sceneGameplay = {}
 local scenesManager = require('scenes.scenesManager')
 
 local player = require('modules.player')
+local ennemi = require('modules.ennemi')
 
 function sceneGameplay.load()
     player.load()
+    ennemi.load()
 end
 
 function sceneGameplay.update(dt)
     player.update(dt)
+    ennemi.update(dt)
 end
 
 function sceneGameplay.draw()
     love.graphics.print('la scene gameplay')
     player.draw()
+    ennemi.draw()
 end
 
 function sceneGameplay.keypressed(key)
